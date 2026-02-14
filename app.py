6d987e5630d928702af01a3b93145210fe11d833
Lixun Peng <PengLiXun@gmail.com>
2026-02-14 14:56:06 +0800

    [Feature] Add Flask application structure
    - Create app.py with Flask application factory
    - Configure SQLAlchemy integration
    - Register blueprints for modular routing

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from config import Config
+from models import db
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+
+@app.route('/')
+def index():
+  """汇总统计页面"""
+  return render_template('index.html')
+
+@app.route('/options')
+def options():
+  """选项维护页面"""
+  return render_template('options.html')
+
+if __name__ == '__main__':
+  app.run(debug=True)
